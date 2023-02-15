a32e973fdb1e7ccd80e4f0373926c98cbc4c250f
Igor Santiago <igor.santiago546@outlook.com>
2023-02-15 01:03:52 -0300

    Apagando Mysql

--- ti92class/Cliente.cs
+++ ti92class/Cliente.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
 using System.Data;
+using System.Security.Cryptography;
+
 namespace ti92class
 {
     public class Cliente
@@ -50,7 +51,7 @@ namespace ti92class
             var cmd = Banco.Abrir();
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = "select * from clientes order by nome asc";
-            var dr = cmd.ExecuteNonQuery();
+            var dr = cmd.ExecuteReader();
             while (dr.Read())
             {
                 lista.Add(new Cliente
@@ -63,7 +64,62 @@ namespace ti92class
                     )
                     );
             }
+            return lista;
         }
+        public static Cliente ObterPorId(int _id)
+        {
+            Cliente cliente = new Cliente();
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from clientes where id =" + _id;
+            var dr = cmd.ExecuteReader();
+            while (dr.Read()) 
+            {
+                cliente.Id = dr.GetString(0);
+                cliente.Cpf = dr.GetInt32(1);
+                cliente.Email = dr.GetString(2);
+                cliente.DataCad = dr.GetDatetime(3);
+                cliente.Ativo = dr.GetBoolean(4);
+            }
+            return cliente;
+        }
+        public static void Atualizar(Cliente cliente)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update clientes set nome = '" + cliente.Nome + "', cpf = " + cliente.Cpf + ",email = '" + cliente.Email + "',datacad = '" + cliente.DataCad + "',ativo = '" + cliente.Ativo;
+            cmd.ExecuteNonQuery();
+        }
+        public bool Excluir(int _id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete * from clientes where id = " + _id;
+            bool result = cmd.ExecuteNonQuery() == 1 ? true : false;
+            return result;
+        }
+        public static List<Cliente> BuscarPorNome(string _parte)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from usuarios where nome like '%" + _parte + "%' order by nome;";
+            var dr = cmd.ExecuteReader();
+            List<Cliente> lista = new List<Cliente>();
+            while (dr.Read()) 
+            {
+                lista.Add(new Cliente
+                    (
+                        dr.GetString(0),
+                        dr.GetInt32(1),
+                        dr.GetString(2),
+                        dr.GetDatetime(3),
+                        dr.GetBoolean(4)
+                    )
+                    );
+            }
+            return lista;
+        }
+
     }
    
 }
