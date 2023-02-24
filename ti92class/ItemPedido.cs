3337714502799a888b4c8521cd47eac71a9aab93
Igor Santiago <igor.santiago546@outlook.com>
2023-02-24 01:42:34 -0300

    Classes Pedido e ItemPedido finalizado

--- ti92class/ItemPedido.cs
+++ ti92class/ItemPedido.cs
@@ -1,21 +1,109 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace ti92class
 {
     public class ItemPedido
     {
-        public Pedido Pedido { get; set; }
+        public int Id { get; set; }
 
         public Produto Produto { get; set; }
 
         public double Preco { get; set; }
 
-        public int Quantidade { get; set; }
+        public double Quantidade { get; set; }
 
         public double Desconto { get; set; }
+
+        public ItemPedido(){}
+
+        public ItemPedido(int id, Produto produto, double preco, double quantidade, double desconto)
+        {
+            this.Id = id;
+            Produto = produto;
+            Preco = preco;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+
+        public ItemPedido(Produto produto, double preco, double quantidade, double desconto)
+        {
+            Produto = produto;
+            Preco = preco;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+        public static List<ItemPedido> Listar(int pedido_id) 
+        {
+            List<ItemPedido> itens = new List<ItemPedido>();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from itempedido where pedido_id = "+ pedido_id;
+            var dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                itens.Add(new ItemPedido(
+                    dr.GetInt32(0),
+                    Produto.ObterPorId(dr.GetInt32(2)),
+                    dr.GetDouble(3),
+                    dr.GetDouble(4),
+                    dr.GetDouble(5)
+                    ));
+            }
+            return itens;
+        }
+        public static ItemPedido BuscarPorProdutoPedido(int pedido_id, int produto_id)
+        {
+            ItemPedido iten = new ItemPedido();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "select * from itempedido where pedido_id = " + pedido_id;
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                iten.Id= dr.GetInt32(0);
+                iten.Produto = Produto.ObterPorId(dr.GetInt32(2));
+                iten.Preco = dr.GetDouble(3);
+                iten.Quantidade = dr.GetDouble(4);
+                iten.Desconto = dr.GetDouble(5);
+            }
+            return iten;
+        }
+        public void Adicionar(int pedido_id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "insert itempedido (pedido_id, produto_id, preco, quantidade, desconto) values (@pedido_id, @produto_id, @preco, @quantidade, @desconto)";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add("@pedido_id",MySqlDbType.Int32).Value = pedido_id;
+            cmd.Parameters.Add("@produto_id", MySqlDbType.Int32).Value = Produto.Id;
+            cmd.Parameters.Add("@preco", MySqlDbType.Decimal).Value = Produto.Preco;
+            cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = Quantidade;
+            cmd.Parameters.Add("@desconto", MySqlDbType.Double).Value = Desconto;
+            cmd.ExecuteNonQuery();
+        }
+        public void Atualizar(int pedido_id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "update itempedido set quantidade = @quantidade, desconto = @desconto " +
+                "where id = @id and pedido_id = @pedido_id";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = Id;
+            cmd.Parameters.Add("@pedido_id", MySqlDbType.Int32).Value = pedido_id;
+            cmd.Parameters.Add("@quantidade", MySqlDbType.Int32).Value = Quantidade;
+            cmd.Parameters.Add("@desconto", MySqlDbType.Double).Value = Desconto;
+            cmd.ExecuteNonQuery();
+        }
+        public void Excluir(int id, int pedido_id)
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandText = "delete from itempedido where id = @id and pedido_id = @pedido_id";
+            cmd.Parameters.Clear();
+            cmd.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
+            cmd.Parameters.Add("@pedido_id", MySqlDbType.Int32).Value = pedido_id;
+            cmd.ExecuteNonQuery();
+        }
     }
 }
--- ti92class/Pedido.cs
+++ ti92class/Pedido.cs
@@ -8,5 +8,77 @@ namespace ti92class
 {
     public class Pedido
     {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+        public double Desconto { get; set; }
+        public Cliente Cliente { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime ArquivadoEm { get; set; }
+        public List<ItemPedido> Itens { get; set; } 
+
+
+        public Pedido()
+        {
+            Usuario = new Usuario();
+            Cliente= new Cliente();
+        }
+
+        
+
+        public Pedido(int id, DateTime data, string status, double desconto, Cliente cliente, Usuario usuario, List<ItemPedido> itens)
+        {
+            Id = id;
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+            Cliente = cliente;
+            Usuario = usuario;
+            Itens = itens;  
+        }
+
+        public Pedido(DateTime data, string status,double desconto, Cliente cliente, Usuario usuario, List<ItemPedido> itens = null)
+        {
+            Data = data;
+            Status = status;
+            Desconto = desconto;
+            Cliente = cliente;
+            Usuario = usuario;
+            Itens = itens;
+        }
+        public void Inserir()
+        {
+
+        }
+        public static List<Pedido> Listar()
+        {
+            List<Pedido> list = new List<Pedido>();
+
+            return list;
+        }
+
+        public static Pedido ObterPorId(int id)
+        {
+            Pedido pedido = new Pedido();
+
+            return pedido;
+        }
+
+        public bool Atualizar()
+        {
+            
+
+            return  false;
+        }
+
+        public void Arquivar(int id)
+        {
+
+        }
+
+        public void Restaurar   (int id)
+        {
+
+        }
     }
 }
